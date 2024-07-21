d6bfdeaef2c7345109020539c6d0673b845d36d3
rishaviitd <ms1210873@iitd.ac.in>
2024-07-22 02:09:09 +0530

    changes

--- src/my_components/buttons/SolidButton.jsx
+++ src/my_components/buttons/SolidButton.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import styles from "./solid.button.module.css";
+import { useNavigate } from "react-router-dom";
+const SolidButton = ({ buttonText, widthPercentage, route }) => {
+  const navigate = useNavigate();
 
-const SolidButton = ({ buttonText, widthPercentage }) => {
+  const handleClick = () => {
+    setTimeout(() => {
+      navigate(route);
+    }, 130); // 0.15 seconds
+  };
   return (
     <button
       className={styles.duolingoButton}
       style={{ width: widthPercentage }}
+      onClick={handleClick}
     >
       <span className={styles.duolingoButtonText}>{buttonText}</span>
     </button>
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -21,26 +21,30 @@ const LoginPage = () => {
       </button>
       <div className="bg-white px-8 rounded w-full max-w-md">
         <h2 className="text-2xl font-bold mb-8 text-center">Log in</h2>
-        <form>
-          <InputField type="email" placeholder="Email" />
-          <InputField type="password" placeholder="Password" />
+        {/* <form> */}
+        <InputField type="email" placeholder="Email" />
+        <InputField type="password" placeholder="Password" />
 
-          <div className="flex justify-between items-center mb-4">
-            <button className="text-blue-500 hover:underline">
-              Forgot Password?
-            </button>
-          </div>
-          <SolidButton buttonText="LOGIN" widthPercentage="100%" />
-          <div className="flex items-center my-4">
-            <div className="flex-grow border-t"></div>
-            <span className="mx-2 text-gray-500">OR</span>
-            <div className="flex-grow border-t"></div>
-          </div>
-          <div className="flex justify-between">
-            <LightButton buttonText="PHONE" widthPercentage="45%" />
-            <LightButton buttonText="GOOGLE" widthPercentage="45%" />
-          </div>
-        </form>
+        <div className="flex justify-between items-center mb-4">
+          <button className="text-blue-500 hover:underline">
+            Forgot Password?
+          </button>
+        </div>
+        <SolidButton
+          buttonText="LOGIN"
+          widthPercentage="100%"
+          route="/student/dashboard"
+        />
+        <div className="flex items-center my-4">
+          <div className="flex-grow border-t"></div>
+          <span className="mx-2 text-gray-500">OR</span>
+          <div className="flex-grow border-t"></div>
+        </div>
+        <div className="flex justify-between">
+          <LightButton buttonText="PHONE" widthPercentage="45%" />
+          <LightButton buttonText="GOOGLE" widthPercentage="45%" />
+        </div>
+        {/* </form> */}
         <div className="mt-4 text-center text-gray-500 text-sm">
           <p>
             By signing in to Mimir, you agree to our{" "}
