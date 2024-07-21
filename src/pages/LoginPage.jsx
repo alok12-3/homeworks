13eb48d4ba2ceee873726b2216cd8f450d6db9f2
rishaviitd <ms1210873@iitd.ac.in>
2024-07-21 18:20:27 +0530

    login check-4

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,35 +2,36 @@ import React from "react";
 import SolidButton from "../my_components/buttons/SolidButton";
 import LightButton from "../my_components/buttons/LightButton";
 import InputField from "../my_components/inputfields/InputField";
+
 const LoginPage = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="bg-white px-8  rounded w-full max-w-md">
-        <div className="text-right">
-          <button className="text-gray-500 hover:text-gray-700">&times;</button>
-        </div>
+    <div className="flex items-center justify-center min-h-screen relative">
+      <button
+        className="absolute top-4 right-4 text-gray-500 hover:text-gray-700 text-3xl"
+        aria-label="Close"
+      >
+        &times;
+      </button>
+      <div className="bg-white px-8 py-4 rounded w-full max-w-md">
         <h2 className="text-2xl font-bold mb-8 text-center">Log in</h2>
         <form>
-          <InputField type="email" placeholder="Email"></InputField>
-          <InputField type="password" placeholder="Password"></InputField>
+          <InputField type="email" placeholder="Email" />
+          <InputField type="password" placeholder="Password" />
 
           <div className="flex justify-between items-center mb-4">
             <button className="text-blue-500 hover:underline">
               Forgot Password?
             </button>
           </div>
-          <SolidButton buttonText="LOGIN" widthPercentage="100%"></SolidButton>
+          <SolidButton buttonText="LOGIN" widthPercentage="100%" />
           <div className="flex items-center my-4">
             <div className="flex-grow border-t"></div>
             <span className="mx-2 text-gray-500">OR</span>
             <div className="flex-grow border-t"></div>
           </div>
           <div className="flex justify-between">
-            <LightButton buttonText="PHONE" widthPercentage="45%"></LightButton>
-            <LightButton
-              buttonText="GOOGLE"
-              widthPercentage="45%"
-            ></LightButton>
+            <LightButton buttonText="PHONE" widthPercentage="45%" />
+            <LightButton buttonText="GOOGLE" widthPercentage="45%" />
           </div>
         </form>
         <div className="mt-4 text-center text-gray-500 text-sm">
