1ffb0a3280fee69a3603dc2e6933d28a08f336ef
rishaviitd <ms1210873@iitd.ac.in>
2024-07-21 16:09:07 +0530

    checks

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -5,6 +5,102 @@ const LandingPage = () => {
     <>
       <Navbar></Navbar>
       <HeroSection></HeroSection>
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt odio
+        repellat nulla tenetur dolor totam nisi veritatis eius eligendi
+        consectetur perspiciatis quis architecto corrupti non mollitia quod
+        natus, quam expedita!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt odio
+        repellat nulla tenetur dolor totam nisi veritatis eius eligendi
+        consectetur perspiciatis quis architecto corrupti non mollitia quod
+        natus, quam expedita!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt odio
+        repellat nulla tenetur dolor totam nisi veritatis eius eligendi
+        consectetur perspiciatis quis architecto corrupti non mollitia quod
+        natus, quam expedita!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt odio
+        repellat nulla tenetur dolor totam nisi veritatis eius eligendi
+        consectetur perspiciatis quis architecto corrupti non mollitia quod
+        natus, quam expedita!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt odio
+        repellat nulla tenetur dolor totam nisi veritatis eius eligendi
+        consectetur perspiciatis quis architecto corrupti non mollitia quod
+        natus, quam expedita!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt odio
+        repellat nulla tenetur dolor totam nisi veritatis eius eligendi
+        consectetur perspiciatis quis architecto corrupti non mollitia quod
+        natus, quam expedita!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt odio
+        repellat nulla tenetur dolor totam nisi veritatis eius eligendi
+        consectetur perspiciatis quis architecto corrupti non mollitia quod
+        natus, quam expedita!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt odio
+        repellat nulla tenetur dolor totam nisi veritatis eius eligendi
+        consectetur perspiciatis quis architecto corrupti non mollitia quod
+        natus, quam expedita!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt odio
+        repellat nulla tenetur dolor totam nisi veritatis eius eligendi
+        consectetur perspiciatis quis architecto corrupti non mollitia quod
+        natus, quam expedita!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt odio
+        repellat nulla tenetur dolor totam nisi veritatis eius eligendi
+        consectetur perspiciatis quis architecto corrupti non mollitia quod
+        natus, quam expedita!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt odio
+        repellat nulla tenetur dolor totam nisi veritatis eius eligendi
+        consectetur perspiciatis quis architecto corrupti non mollitia quod
+        natus, quam expedita!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt odio
+        repellat nulla tenetur dolor totam nisi veritatis eius eligendi
+        consectetur perspiciatis quis architecto corrupti non mollitia quod
+        natus, quam expedita!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt odio
+        repellat nulla tenetur dolor totam nisi veritatis eius eligendi
+        consectetur perspiciatis quis architecto corrupti non mollitia quod
+        natus, quam expedita!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt odio
+        repellat nulla tenetur dolor totam nisi veritatis eius eligendi
+        consectetur perspiciatis quis architecto corrupti non mollitia quod
+        natus, quam expedita!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt odio
+        repellat nulla tenetur dolor totam nisi veritatis eius eligendi
+        consectetur perspiciatis quis architecto corrupti non mollitia quod
+        natus, quam expedita!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt odio
+        repellat nulla tenetur dolor totam nisi veritatis eius eligendi
+        consectetur perspiciatis quis architecto corrupti non mollitia quod
+        natus, quam expedita!
+      </h1>
     </>
   );
 };
