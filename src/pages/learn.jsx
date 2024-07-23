485df363e40bae99f0296fc66e89a61473742eb3
rishaviitd <ms1210873@iitd.ac.in>
2024-07-23 21:15:21 +0530

    changes

--- src/pages/learn.jsx
+++ src/pages/learn.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Learn = () => {
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold ">Learn</h1>
+      <h1 className="text-2xl font-bold pb-2">Learn</h1>
       <div className="space-y-4">
         <div className="bg-white p-4 rounded shadow">
           Content 1 Content 1 Content 1 Content 1
