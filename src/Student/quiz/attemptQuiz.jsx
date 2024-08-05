fc81a2cc8e01b2501749de44310ca6fd184b25d1
rishaviitd <ms1210873@iitd.ac.in>
2024-08-05 14:30:01 +0530

    rishav made some files of attempt and review quiz

---
+++ src/Student/quiz/attemptQuiz.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function attemptQuiz() {
+  return <div></div>;
+}
+
+export default attemptQuiz;
--- src/Tester.jsx
+++ src/Tester.jsx
@@ -13,23 +13,23 @@ import {
 
 function App() {
   return (
-    // <div className="app-background">
-    //   <Navbar />
-    //   <Blankdiv />
-    //   <div className="main-content">
-    //     <Blankdiv />
-    //   </div>
-    // </div>
-    <Tabs defaultValue="account" className="w-[400px]">
-      <TabsList>
-        <TabsTrigger value="account">Account</TabsTrigger>
-        <TabsTrigger value="password">Password</TabsTrigger>
-      </TabsList>
-      <TabsContent value="account">
-        Make changes to your account here.
-      </TabsContent>
-      <TabsContent value="password">Change your password here.</TabsContent>
-    </Tabs>
+    <div className="app-background">
+      <Navbar />
+      <Blankdiv />
+      <div className="main-content">
+        <Blankdiv />
+      </div>
+    </div>
+    // <Tabs defaultValue="account" className="w-[400px]">
+    //   <TabsList>
+    //     <TabsTrigger value="account">Account</TabsTrigger>
+    //     <TabsTrigger value="password">Password</TabsTrigger>
+    //   </TabsList>
+    //   <TabsContent value="account">
+    //     Make changes to your account here.
+    //   </TabsContent>
+    //   <TabsContent value="password">Change your password here.</TabsContent>
+    // </Tabs>
   );
 }
 
